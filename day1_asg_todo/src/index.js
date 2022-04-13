6b92b4f94847fabbf453a0b4f1924e529cbb94a4
viraj gupta <97445928+rkvirajgupta@users.noreply.github.com>
2022-04-13 11:57:17 +0530

    added

---
+++ day1_asg_todo/src/index.js
@@ -0,0 +1,55 @@
+import "./index.css";
+import todo from "./todo.jpg";
+
+const root = document.getElementById("root");
+
+// Navbar
+
+const navbar = document.createElement("div");
+navbar.id = "nav";
+
+const title = document.createElement("p");
+title.textContent = "Todo App";
+title.id = "title";
+
+const image = document.createElement("img");
+image.src = todo;
+image.id = "image";
+
+// Container
+
+const container = document.createElement("div");
+container.id = "container";
+
+const leftDiv = document.createElement("div");
+leftDiv.id = "left";
+const rightDiv = document.createElement("div");
+rightDiv.id = "right";
+
+// Left Div
+
+const input = document.createElement("textarea");
+input.placeholder = "Enter todo text"
+const btn = document.createElement("button");
+btn.id = "btn";
+btn.textContent = "Add";
+
+// Right Div
+
+btn.addEventListener("click", () => {
+  let todo = input.value;
+  console.log(todo);
+
+  const list = document.createElement("div");
+  list.id = "list";
+
+  const p = document.createElement("p");
+  p.textContent = todo;
+  list.append(p);
+  rightDiv.append(list);
+});
+
+navbar.append(image, title);
+leftDiv.append(input, btn);
+container.append(leftDiv, rightDiv);
+root.append(navbar, container);
