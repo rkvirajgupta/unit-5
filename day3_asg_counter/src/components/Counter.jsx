a20016e2986a8c44f2c367b67f7b8ffc1118ca73
viraj gupta <97445928+rkvirajgupta@users.noreply.github.com>
2022-04-13 15:11:06 +0530

    added

---
+++ day3_asg_counter/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Counter = () => {
+   const [count, setCount] = useState(0);
+
+   const handleChange = (value) => {
+      if (value === 2) {
+         setCount(count * value);
+      }
+      else {
+         setCount(count + value);
+      }
+   }
+
+   return (
+      <div>
+         <h1>Counter</h1>
+         <h2 className={count % 2 === 0 ? "green" : "red"}>{count}</h2>
+        <div className="flex"> <button onClick={() => handleChange(1)}>Add</button>
+         <button onClick={() => handleChange(-1)}>Subtract</button>
+         <button onClick={() => handleChange(2)}>Double</button></div>
+      </div>
+   )
+}
+
+export { Counter };
