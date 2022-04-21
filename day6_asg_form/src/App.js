df3064278b77474a334ccacdd51b47132c4703aa
viraj gupta <97445928+rkvirajgupta@users.noreply.github.com>
2022-04-21 23:45:27 +0530

    added

---
+++ day6_asg_form/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/Table';
+import Form from './components/Form'
+function App() {
+  return (
+    <div className="App">
+     <Table/>
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day6_asg_form/src/components/Table.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+const Table = () => {
+   const [data, setData] = useState([]);
+
+   const getData = async () => {
+      let fetched = await fetch("http://localhost:8080/data");
+      fetched = await fetched.json();
+      console.log(fetched);
+      setData([...data, ...fetched]);
+   };
+
+   useEffect(() => {
+      getData();
+   }, [setData]);
+
+   return (
+      <div className="tableDiv" style={{border:"1px solid red"}}>
+         <h1>Employees</h1>
+         <table>
+            <thead>
+               <tr>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Address</th>
+                  <th>Department</th>
+                  <th>Salary</th>
+                  <th>Martial Status</th>
+               </tr>
+            </thead>
+            <tbody>
+               {data.map((el) => {
+                  return (
+                     <tr>
+                        <td>{el.name}</td>
+                        <td>{el.age}</td>
+                        <td>{el.address}</td>
+                        <td>{el.department}</td>
+                        <td>{el.salary}</td>
+                        <td>{el.status}</td>
+                     </tr>
+                  );
+               })}
+            </tbody>
+         </table>
+      </div>
+   );
+};
+
+export default Table;
