edc10b525e04356ee79296d45cc7b1913d8b145d
viraj gupta <97445928+rkvirajgupta@users.noreply.github.com>
2022-04-26 16:54:52 +0530

    added

---
+++ day9_asg_auth_context/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import { Showstatus } from './components/Showstatus';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Showstatus/>
+    </div>
+  );
+}
+
+export default App;
